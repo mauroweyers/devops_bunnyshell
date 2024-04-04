5f31a77c4360226bab5968f11bf495bd817f955e
Mauro <mr.weyers@student.avans.nl>
2024-04-05 01:11:11 +0200

    debugging for production test

--- week1/services/database.js
+++ week1/services/database.js
@@ -5,13 +5,24 @@ let connectionString = ''
 const databasename = process.env.DB_NAME
 
 if (process.env.NODE_ENV !== 'test') {
+  console.log('not testing')
+
+
   const databaseUsername = process.env.DB_USERNAME
   const databasePassword = process.env.DB_PASSWORD
   const databaseHostname = process.env.MONGO_URL
   connectionString = `mongodb://${databaseUsername}:${databasePassword}@${databaseHostname}/`
+
+
+
+
 } else {
+  console.log('testing')
   connectionString = process.env.MONGO_URL
+
 }
+console.log("connection string:")
+  console.log(connectionString)
 
 const client = new MongoClient(connectionString)
 const db = client.db(databasename)
