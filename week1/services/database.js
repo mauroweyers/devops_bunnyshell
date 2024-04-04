7ab34d7178792d592fab1c2ba726d38cf36ffc60
Mauro <mr.weyers@student.avans.nl>
2024-04-04 18:47:07 +0200

    Linting fixes for mongodb config

--- week1/services/database.js
+++ week1/services/database.js
@@ -1,14 +1,14 @@
 require('dotenv').config()
 const { MongoClient } = require('mongodb')
 
-let connectionString = ""
+let connectionString = ''
 const databasename = process.env.DB_NAME
 
-if (process.env.NODE_ENV != 'test') {
+if (process.env.NODE_ENV !== 'test') {
   const databaseUsername = process.env.DB_USERNAME
   const databasePassword = process.env.DB_PASSWORD
   const databaseHostname = process.env.MONGO_URL
-  connectionString = `mongodb://${databaseUsername}:${databasePassword}@${databaseHostname}/`;
+  connectionString = `mongodb://${databaseUsername}:${databasePassword}@${databaseHostname}/`
 } else {
   connectionString = process.env.MONGO_URL
 }
